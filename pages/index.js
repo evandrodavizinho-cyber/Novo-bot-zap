098d67b5c2730d71ef2f8c029d946006418332d3
evandrodavizinho-cyber <evandro.davizinho@gmail.com>
2026-04-12 21:10:27 -0300

    Add Home component for main page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Novo Bot Zap 🤖</h1>
+      <p>Seu bot de WhatsApp com IA está online 🚀</p>
+    </div>
+  );
+  }
